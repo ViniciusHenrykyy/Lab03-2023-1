3da95273c73762a8e1ef77e52c1fe5c1ec1fd115
ViniciusHenrykyy <137112333+ViniciusHenrykyy@users.noreply.github.com>
2023-11-12 17:45:54 -0300

    Atividade concluída

--- src/main/linked/ListaLigada.java
+++ src/main/linked/ListaLigada.java
@@ -41,62 +41,205 @@ public class ListaLigada implements EstruturaElementar{
     }
 
     @Override
-    public int minimo() {return 1;
+    public int minimo(){
+        if(this.cabeca == null){   
+            return -1;
+        }else{
+            No mao = this.cabeca;
+            int valorminimo = mao.getValor();
+            while(true){
+              No mochila_dentro_mochila = mao.getProximo();
+              if(mochila_dentro_mochila == null){
+                break;
+              }else{
+                if(mochila_dentro_mochila.getValor() < valorminimo){
+                    valorminimo = mochila_dentro_mochila.getValor();
+                }
+                mao = mochila_dentro_mochila;
+              }
+            }
+            return valorminimo;
+        }
     }
 
     @Override
-    public int maximo() {return 1;
+    public int maximo(){
+        if(this.cabeca == null){   
+            return -1;
+        }else{
+            No mao = this.cabeca;
+            int valormaximo = mao.getValor();
+            while(true){
+              No mochila_dentro_mochila = mao.getProximo();
+              if(mochila_dentro_mochila == null){
+                break;
+              }else{
+                if(mochila_dentro_mochila.getValor() > valormaximo){
+                    valormaximo = mochila_dentro_mochila.getValor();
+                }
+                mao = mochila_dentro_mochila;
+              }
+            }
+            return valormaximo;
+        }
+    }
+    
+
+    @Override
+    public int predecessor(int valor) {
+        return buscaIndice(valor - 1);
     }
 
     @Override
-    public int predecessor(int valor) {return 1;
-    }
-
-    @Override
-    public int sucessor(int valor) {return 1;
+    public int sucessor(int valor) {
+        int contador = 0;
+        No mao = this.cabeca;
+        No mochila_dentro_mochila = mao.getProximo();
+        while(mochila_dentro_mochila != null){
+            if(contador == valor){
+                return mochila_dentro_mochila.getValor();
+            }
+            contador++;
+            mao = mao.getProximo();
+            mochila_dentro_mochila = mao.getProximo();   
+        }
+        return -1;
     }
 
     @Override
     public void insereElemento(int valor) {
+        insereInicio(valor);
+    }
+
+    @Override // [Erick, vinicius, Glad]
+    public void insereElementoPosicao(int valor, int buscaIndice) {
+        No mochila = new No(valor);
+        if (cabeca == null){
+            this.cabeca = mochila;
+        }else{
+            if(buscaIndice == 0){
+                mochila.setProximo(this.cabeca);
+                this.cabeca = mochila;
+
+            }
+            int contador = 1;
+            No mao = this.cabeca;
+            No mochila_dentro_mochila = mao.getProximo();
+            while(mochila_dentro_mochila != null){
+                if(contador == buscaIndice){
+                   mochila.setProximo(mochila_dentro_mochila);
+                   mao.setProximo(mochila);
+                   return; 
+                }
+                contador++;
+                mao = mao.getProximo();
+                mochila_dentro_mochila = mao.getProximo();
+  
+            }
+        }        
     }
 
     @Override
-    public void insereElementoPosicao(int valor, int buscaIndice) {
-
-        }
-
-    @Override
     public void insereInicio(int valor) {
+        No mochila = new No(valor);
         if (this.cabeca == null){
-        this.cabeca = new No(valor);
+            this.cabeca = mochila;
         }else{
-            No n = new No(valor);
-            n.setProximo(this.cabeca);
-            this.cabeca = n;
+            mochila.setProximo(this.cabeca);
+            this.cabeca = mochila;
         }
     }
 
     @Override
     public void insereFim(int valor) {
-
-        
-
+        No mochila = new No(valor);
+        if (this.cabeca == null){
+            this.cabeca = mochila;  
+        }else{
+            No mao = this.cabeca;
+            while(true){
+                No mochila_dentro_mochila = mao.getProximo();
+                if(mochila_dentro_mochila == null){
+                    mao.setProximo(mochila);
+                    break;
+                }else{
+                    mao = mochila_dentro_mochila;
+                }  
+            }
+        }    
     }
 
     @Override
     public void remove(int valor) {
+        if(this.cabeca != null){
+            No mao = this.cabeca;
+            if(mao.getValor() == valor){
+                this.cabeca = this.cabeca.getProximo();
+                return;
+            }
+            while(true){
+                 No mochila_dentro_mochila = mao.getProximo();
+                if(mochila_dentro_mochila == null){
+                    break;
+                }
+                if(mochila_dentro_mochila.getValor() == valor){
+                    mao.setProximo(null);
+                    break;
+                }else{
+                    mao = mochila_dentro_mochila;
+                }                                   
+            }
+        }      
     }
 
-    @Override
+    @Override //[Erick, Vinicius, Gui, Glad]
     public void removeIndice(int indice) {
+             if (cabeca == null){
+            return;
+        } 
+        if(indice == 0){
+            removeInicio();
+            return;
+        }
+        int contador = 0;
+        No mao = this.cabeca;
+        No mochila_dentro_mochila = mao.getProximo();
+        while(mochila_dentro_mochila != null){
+            if (contador == indice){
+                mao.setProximo(mochila_dentro_mochila.getProximo());
+                return;
+            }
+            contador++;
+            mao = mao.getProximo();
+            mochila_dentro_mochila = mao.getProximo();
+        }
     }
 
     @Override
     public void removeInicio() {
+        if(this.cabeca != null){
+            No mao = this.cabeca;
+            No mochila_dentro_mochila = mao.getProximo();
+            this.cabeca = mochila_dentro_mochila;
+        }
     }
-
+    //[5,4,3,2,1]
     @Override
     public void removeFim() {
+        if(this.cabeca != null){//n
+            No mao = this.cabeca;//mão é = 5
+            while(true){//mão = 5 // loop // mão = 4// mão = 3// mão = 2// 
+                No mochila_dentro_mochila = mao.getProximo();// 4 // 3// 2 // 1
+                if(mochila_dentro_mochila == null){// ñ // ñ // ñ // ñ //
+                    break;
+                }
+                if(mochila_dentro_mochila.getProximo() == null){// 3 e ñ// 2 e ñ// 1 e ñ//null sim //
+                    mao.setProximo(null);
+                    break;
+                }else{
+                    mao = mochila_dentro_mochila;// 4 // 3 // 2 //
+                }
+            }  
+        }
     }
-    
 }
